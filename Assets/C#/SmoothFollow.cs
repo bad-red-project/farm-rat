1e26e1e3f974ff2974b4acfcddbc762ac9a39013
koroldavid <koroldavid08@gmail.com>
2021-02-07 20:56:48 +0200

    Add smoothig camera

---
+++ Assets/C#/SmoothFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour {
+
+    public float smoothing = 0.5f;
+    public Transform target;
+
+    void LateUpdate() {
+        if (target == null && transform.position != target.position) { return; }
+
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+    }
+}
