66c336dd01076dfed29aae0f8b1f5f01e11dd978
Burenko <burenkoanton@gmail.com>
2021-02-08 23:33:01 +0100

    create scenes, cut house models, create scripts scratches

---
+++ Assets/C#/LoadNewScene.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+
+public class LoadNewScene : MonoBehaviour
+{
+    public string levelToLoad;
+    public long millisBeforeEnter;
+    //private Stopwatch
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "player")
+        {
+            //if (stayingTime > millisBeforeEnter) {
+            //    enterNewLocation(collision);
+            //}
+        }
+    }
+    
+    private void EnterNewLocation(Collider2D collision) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelToLoad);
+    }
+}
