976857f99a7a2b55033950bb6a16a282c7e48203
Burenko <burenkoanton@gmail.com>
2021-02-21 20:44:32 +0100

    add transition between scenes when colliding

--- Assets/C#/LoadNewScene.cs
+++ Assets/C#/LoadNewScene.cs
@@ -4,8 +4,8 @@ using System.Diagnostics;
 public class LoadNewScene : MonoBehaviour
 {
     public string levelToLoad;
-    public long millisBeforeEnter;
-    //private Stopwatch
+    public long neededStayTimeMillis;
+    private Stopwatch timeStayed = new Stopwatch();
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +21,36 @@ public class LoadNewScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if (collision.gameObject.name == "PlayersBoxCollider")
+        {
+            UnityEngine.Debug.Log("player entered");
+            timeStayed.Start();
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "player")
+        if (collision.gameObject.name == "PlayersBoxCollider")
         {
-            //if (stayingTime > millisBeforeEnter) {
-            //    enterNewLocation(collision);
-            //}
+            TryEnterNewLocation();
         }
     }
-    
-    private void EnterNewLocation(Collider2D collision) {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(levelToLoad);
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "PlayersBoxCollider")
+        {
+            timeStayed.Stop();
+            TryEnterNewLocation();
+            timeStayed.Reset();
+        }
+    }
+
+    private void TryEnterNewLocation() {
+        long stayTime = timeStayed.ElapsedMilliseconds;
+        if (stayTime > neededStayTimeMillis)
+        {
+             UnityEngine.SceneManagement.SceneManager.LoadScene(levelToLoad);
+        }
     }
 }
