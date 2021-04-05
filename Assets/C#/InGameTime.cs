e90d56b6a429459a98a905897ebf17204b1ea247
Burenko <burenkoanton@gmail.com>
2021-04-05 19:53:44 +0200

    standartize time icon, change fonts, add day time bias, fix bugs

--- Assets/C#/InGameTime.cs
+++ Assets/C#/InGameTime.cs
@@ -4,29 +4,35 @@ using UnityEngine.UI;
 public class InGameTime : MonoBehaviour
 {
     public double minuteDurationInSeconds = 5.0;
+    // can be used to reduce time needed for the day time to last shorter or longer
+    private double dayTimeBias = 0;
 
     private double lastChange = 0.0;
 
     // represents hours from 0 to 23
     private int hour = 5;
     // represents minutes from 0 to 5 e.g. meaning 0 and 50 minutes 
-    private int minutes = 5;
+    private int minutes = 4;
 
     private Text clockLabel;
-    private Color colorDay = new Color(0, 0.5f, 1);
-    private Color colorNight = new Color(0, 0, 1);
+
+    private int dayBegin = 6;
+    private int dayEnd = 21;
+    private Color colorDay = new Color(0.8f, 0.8f, 1);
+    private Color colorNight = new Color(0.2f, 0.2f, 0.2f);
 
     // Start is called before the first frame update
     void Start()
     {
         clockLabel = GetComponent<Text>();
+        UpdateDayTime();
         ShowInGUI();
     }
 
 
     void Update()
     {
-        if ((Time.time - lastChange) < minuteDurationInSeconds) 
+        if ((Time.time - lastChange) < minuteDurationInSeconds + dayTimeBias) 
         {
             return;
         }
@@ -42,15 +48,7 @@ public class InGameTime : MonoBehaviour
             }
         }
 
-        if (hour > 22 && hour < 6)
-        {
-            clockLabel.color = colorNight;
-        }
-        else
-        {
-            clockLabel.color = colorDay;
-        }
-
+        UpdateDayTime();
         ShowInGUI();
         lastChange = Time.time;
     }
@@ -58,5 +56,21 @@ public class InGameTime : MonoBehaviour
     void ShowInGUI()
     {
         clockLabel.text = hour.ToString().PadLeft(2,'0') + ":" + minutes.ToString().PadRight(2, '0');
+    }
+
+    void UpdateDayTime() 
+    {
+        bool isDay = hour >= dayBegin && hour < dayEnd;
+        if (isDay)
+        {
+            clockLabel.color = colorDay;
+            dayTimeBias = 0;
+        }
+        else
+        {
+            clockLabel.color = colorNight;
+            // makes night shorter
+            dayTimeBias = -minuteDurationInSeconds*0.8;
+        }
     }
 }
