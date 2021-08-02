be338b251f578a8127dc87da9156c1e9acc04ca0
koroldavid <koroldavid08@gmail.com>
2021-08-02 17:47:13 +0300

    Create Items database

---
+++ Assets/C#/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Assets/Item")]
+public class Item: ScriptableObject
+{
+    [Serializable]
+    public struct ItemStats
+    {
+        public int attackPover;
+        public int defence;
+    }
+
+    public string id;
+    public string title;
+    [TextArea]
+    public string description;
+    public Sprite icon;
+    public ItemStats stats;
+
+    public Item(Item item)
+    {
+        id = item.id;
+        title = item.title;
+        description = item.description;
+        icon = item.icon;
+        stats = item.stats;
+    }
+}
---
+++ Assets/C#/ItemDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Database", menuName = "Assets/Database/Item Database")]
+public class ItemDatabase : ScriptableObject
+{
+    public List<Item> allItems;
+}
